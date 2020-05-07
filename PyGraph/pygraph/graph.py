3e9e6aa6fae5795900f6dac592d9418797e95fcd
Hanoch Rahimi <hanoch.rahimi@gmail.com>
2020-05-07 14:15:41 +0300

    wip

--- PyGraph/pygraph/graph.py
+++ PyGraph/pygraph/graph.py
@@ -3,15 +3,21 @@ import abc
 class Node:
     """description of class"""
 
-    def __init__(self, id, nodeObj = None):
+    def __init__(self, node_data = {}, id = ""):
         self.id = id
-        self.org_obj = nodeObj
+        self.data = node_data
 
 
 class Edge:
     """description of class"""
 
-    def __init__(self, id):
+    def __init__(self, source_node_id:str, target_node_id:str, edge_data = {}, id = ""):
+        self.source_id = source_node_id
+        self.target_id = target_node_id
+        self.data = edge_data
+        if 'source_id' not in self.data:
+            self.data['source_id'] = self.source_id
+            self.data['target_id'] = self.target_id
         self.id = id
 
 
